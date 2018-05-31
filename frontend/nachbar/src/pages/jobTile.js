59d47e47ef602944a0553458f46c9f457b1972b9
Duc Ha <ha@campus.tu-berlin.de>
2018-05-31 22:52:19 +0200

    small restructuring

--- frontend/nachbar/src/pages/jobTile.js
+++ frontend/nachbar/src/pages/jobTile.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
-
+import CardMedia from '@material-ui/core/CardMedia';
+import Houses from '../images/houses.jpg';
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -17,9 +18,12 @@ const styles = theme => ({
     color: theme.palette.text.secondary,
     height: "400px"
   },
+  tilebar:{
+    height: '25%',
+  },
   content: {
     paddingTop: theme.spacing.unit * 2
-  }
+  },
 });
 
 class JobTile extends React.Component {
@@ -30,20 +34,40 @@ class JobTile extends React.Component {
     const { classes } = this.props;
     const title = this.props.title;
 
+    const tileBar = <AppBar
+        className={classes.tilebar}
+        position="static"
+        color="default"
+        >
+        <Toolbar>
+          <div style={{padding:'16px'}}>
+            <Typography variant="title" color="inherit" gutterBottom>
+              {title}
+            </Typography>
+            <Typography
+              variant="subheading"
+              color="inherit"
+              align="left"
+            >
+              {'von Author'}
+            </Typography>
+          </div>
+        </Toolbar>
+      </AppBar>;
+
     return (
-      <Grid item xs={12} sm={6} lg={4}>
-        <Paper className={classes.paper}>
-          <AppBar position="static" color="default">
-            <Toolbar>
-              <Typography variant="title" color="inherit">
-                {title}
-              </Typography>
-            </Toolbar>
-          </AppBar>
+      <Grid item xs={12} md={6} lg={4}>
+        <Card className={classes.paper} elevation={4}>
+          <CardMedia
+            className={classes.media}
+            src={Houses}
+            title="Test Img"
+          />
+          {tileBar}
           <Typography className={classes.content} variant="body1" color="inherit">
             Beispiel Content
           </Typography>
-        </Paper>
+        </Card>
       </Grid>
     );
   }
